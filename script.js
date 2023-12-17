4dd7fed7785f71ba6c5d7ad6a0a3edaf9c660334
Van Loc Doan <vaan.l.doan@gmail.com>
2023-12-17 11:17:30 +0100

    cart-display and total price

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import { showCatCount } from './script/db.js';
 import { createItemCard } from './script/cards.js';
+import { cartModal } from './script/cartDisplay.js';
 
 showCatCount();
 createItemCard();
+
+cartModal();
