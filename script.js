06ae5c9be16eeb91ea8b57060caed71270ee4084
Moosees <linus.almgren@gmail.com>
2023-12-18 09:43:37 +0100

    add back modal functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { createItemCards } from './script/cards.js';
+import { cartModal } from './script/cartDisplay.js';
 import { printCatCount, printFeaturedItems, printMenu } from './script/db-utils.js';
 import { menu } from './script/menu.js';
 import { searchForTermInMenu } from './script/search.js';
@@ -8,3 +9,4 @@ printFeaturedItems(menu);
 printMenu(menu);
 console.log(searchForTermInMenu('dUcK', menu));
 createItemCards(menu);
+cartModal();
