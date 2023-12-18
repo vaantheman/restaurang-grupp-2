22aa67dd070bce6b3084409125f4c1b2dddac84c
Van Loc Doan <vaan.l.doan@gmail.com>
2023-12-18 21:32:10 +0100

    move add to local storage functions to a separate file

--- script/addToCartBtn.js
+++ script/addToCartBtn.js
@@ -1,4 +1,4 @@
-import { cartItemsDisplay } from './cartDisplay.js';
+import { addToStorage } from './addToStorage.js';
 
 export const addToCartButton = (item) => {
     const addToCartBtn = document.createElement('button');
@@ -7,20 +7,4 @@ export const addToCartButton = (item) => {
     addToCartBtn.addEventListener('click', () => addToStorage(item));
 
     return addToCartBtn;
-};
-
-export const addToStorage = (item) => {
-    const cartItem = { name: item.name, price: item.price };
-    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-    cartItems.push(cartItem);
-
-    const itemQuantity = cartItems.find((cartItem) => cartItem.name === item.name);
-    if (itemQuantity) {
-        itemQuantity.quantity = (itemQuantity.quantity || 0) + 1;
-    } else {
-        cartItems.push({ ...item, quantity: 1 });
-    }
-
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-    cartItemsDisplay();
 };
