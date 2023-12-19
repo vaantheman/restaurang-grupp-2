51928f6e8235cacf5b5069bb82e4ccc019053d47
Moosees <linus.almgren@gmail.com>
2023-12-19 21:57:34 +0100

    add comments to make script.js easier to read

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 import { createItemCards } from './script/cards.js';
 import { cartModal } from './script/cartDisplay.js';
-import { printCatCount, printFeaturedItems, printMenu } from './script/db-utils.js';
 import { menu } from './script/menu.js';
-import { searchForTermInMenu } from './script/search.js';
 import { handleUrlParams } from './script/url.js';
 
+/* APP SETUP */
+handleUrlParams();
+cartModal();
+createItemCards(menu); // should only show featured items
+
+/* DEBUGGING */
+//import { printCatCount, printFeaturedItems, printMenu } from './script/db-utils.js';
 // printCatCount(menu);
 // printFeaturedItems(menu);
 // printMenu(menu);
-handleUrlParams();
-console.log(searchForTermInMenu('dUcK', menu));
-createItemCards(menu);
-cartModal();
