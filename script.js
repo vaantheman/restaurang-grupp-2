3aa0af3d4451f42e846b4114b3c8e147261a8a36
Moosees <linus.almgren@gmail.com>
2023-12-19 21:52:59 +0100

    update url params function to set table number to local storage and hide params on page load

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ import { cartModal } from './script/cartDisplay.js';
 import { printCatCount, printFeaturedItems, printMenu } from './script/db-utils.js';
 import { menu } from './script/menu.js';
 import { searchForTermInMenu } from './script/search.js';
-import { getTableNumber } from './script/url.js';
+import { handleUrlParams } from './script/url.js';
 
 // printCatCount(menu);
 // printFeaturedItems(menu);
 // printMenu(menu);
-console.log(getTableNumber());
+handleUrlParams();
 console.log(searchForTermInMenu('dUcK', menu));
 createItemCards(menu);
 cartModal();
--- script/url.js
+++ script/url.js
@@ -1,3 +1,9 @@
-export const getTableNumber = () => {
-	return new URLSearchParams(window.location.search).get('table');
+export const handleUrlParams = () => {
+	const params = new URLSearchParams(location.search);
+
+	if (params.has('table')) {
+		localStorage.setItem('table', params.get('table'));
+		history.replaceState(null, '', 'index.html');
+	}
+
 };
