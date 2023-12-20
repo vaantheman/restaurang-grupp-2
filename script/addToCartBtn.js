d53d38b550470fe1171008690d982b2811cfed6b
Van Loc Doan <vaan.l.doan@gmail.com>
2023-12-20 18:42:13 +0100

    add layout

--- script/addToCartBtn.js
+++ script/addToCartBtn.js
@@ -2,7 +2,8 @@ import { addToLocalStorage } from './localStorage.js';
 
 export const addToCartButton = (item) => {
     const addToCartBtn = document.createElement('button');
-    addToCartBtn.innerText = 'Place Order';
+    addToCartBtn.className = 'item-container__btn';
+    addToCartBtn.innerText = 'Add To Cart';
 
     addToCartBtn.addEventListener('click', () => {
         addToLocalStorage(item);
