2c0f81df7113f4ba5ad0d90a082ef2cc806f50a0
Moosees <linus.almgren@gmail.com>
2023-12-21 13:51:48 +0100

    add navigation to and from order confirmed page

--- script/buttons.js
+++ script/buttons.js
@@ -2,16 +2,27 @@ import { createItemCards } from "./cards.js";
 import { addToLocalStorage } from './localStorage.js';
 import { menu } from "./menu.js";
 import { searchForCategoryInMenu } from "./search.js";
+import { infoText } from "./text.js";
+
+const starters = document.getElementById('starters');
+const bbqs = document.getElementById('bbqs');
+const drinks = document.getElementById('drinks');
+const categories = document.getElementById('categories');
+const confirm = document.getElementById('confirmOrder');
+const heading = document.querySelector('.content__heading');
+const text = document.querySelector('.content__text');
+const search = document.querySelector('.search-container');
+const itemWrapper = document.getElementById('item-wrapper');
+const modal = document.querySelector('[data-modal]');
+const toMenuBtn = document.getElementById('to-menu-btn');
+const openModalBtn = document.querySelector('[data-open-modal]');
+const addToCartBtn = document.createElement('button');
 
 const handleCategoryClick = (cat) => {
 	createItemCards(searchForCategoryInMenu(cat, menu));
 };
 
 export const setUpCategoriesButtons = () => {
-	const starters = document.getElementById('starters');
-	const bbqs = document.getElementById('bbqs');
-	const drinks = document.getElementById('drinks');
-
 	starters.addEventListener('click', () => handleCategoryClick('starters'));
 
 	bbqs.addEventListener('click', () => handleCategoryClick('bbqs'));
@@ -20,7 +31,6 @@ export const setUpCategoriesButtons = () => {
 };
 
 export const setUpAddToCartButton = (item) => {
-	const addToCartBtn = document.createElement('button');
 	addToCartBtn.className = 'item-container__btn';
 	addToCartBtn.innerText = 'Add To Cart';
 
@@ -32,26 +42,31 @@ export const setUpAddToCartButton = (item) => {
 };
 
 export const setUpConfirmOrderButton = () => {
-	const confirm = document.getElementById('confirmOrder');
+	confirm.addEventListener('click', () => {
+		heading.innerText = 'Tack för din beställning!';
+		text.innerText = infoText;
 
-	confirm.addEventListener('click', () => alert('Your order have been placed!'));
+		toMenuBtn.classList.remove('hidden');
+		categories.classList.add('hidden');
+		openModalBtn.classList.add('hidden');
+		search.classList.add('hidden');
+		heading.classList.remove('hidden');
+		text.classList.remove('hidden');
+		itemWrapper.classList.add('hidden');
+
+		modal.close();
+	});
 };
 
 export const setUpToMenuButton = () => {
-	const toMenu = document.getElementById('to-menu-btn');
-	const categories = document.getElementById('categories');
-	const openModalBtn = document.querySelector('[data-open-modal]');
-	const heading = document.querySelector('.content__heading');
-	const text = document.querySelector('.content__text');
-	const search = document.querySelector('.search-container');
-
-	toMenu.addEventListener('click', () => {
-		toMenu.classList.add('hidden');
+	toMenuBtn.addEventListener('click', () => {
+		toMenuBtn.classList.add('hidden');
 		categories.classList.remove('hidden');
 		openModalBtn.classList.remove('hidden');
 		search.classList.remove('hidden');
 		heading.classList.add('hidden');
 		text.classList.add('hidden');
+		itemWrapper.classList.remove('hidden');
 
 		createItemCards(menu);
 	});
--- script/text.js
+++ script/text.js
@@ -1,1 +1,4 @@
-export const infoText = `Välkommen till Taverna! Vår passion är att skapa en autentisk BBQ-upplevelse, där varje måltid är en hyllning till rökiga smaker och traditionella tillagningsmetoder. Vårt kök är fyllt av en blandning av hemligheter och kärlek till konsten att grilla. Oavsett om du älskar saftiga revben, långsamt tillagad pulled pork eller smakrik brisket, kommer du att uppleva äkta värme i varje tugga. På Taverna handlar det inte bara om mat; det handlar om gemenskap, glädje och en smakresa genom BBQ-traditioner. Vi ser fram emot att välkomna dig till vårt bord och dela dessa smakupplevelser med dig.`;
+export const infoText =
+	`Vår passion är att skapa en autentisk BBQ-upplevelse, där varje måltid är en hyllning till rökiga smaker och traditionella tillagningsmetoder. 
+	Vårt kök är fyllt av en blandning av hemligheter och kärlek till konsten att grilla. Oavsett om du älskar saftiga revben, långsamt tillagad pulled pork eller smakrik brisket, kommer du att uppleva äkta värme i varje tugga. 
+	På Rök & Rull BBQ handlar det inte bara om mat; det handlar om gemenskap, glädje och en smakresa genom BBQ-traditioner. `;
